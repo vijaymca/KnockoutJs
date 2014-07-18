9f58a69cb49ef8c3090e1ba3c0518eef4227fcf9
Vijay Bhaskar <vijaymca@sify.com>
2014-07-18 20:22:41 +0530

    knockout
    repeat working..

--- Examples/knockoutBasic.js
+++ Examples/knockoutBasic.js
@@ -3,4 +3,24 @@ var myViewModel = {
     personAge: ko.observable(23)
 };
 
-ko.applyBindings(myViewModel);
+//ko.applyBindings(myViewModel);
+
+
+
+function AppViewModel() {
+    var self = this;
+    self.firstName = ko.observable('Vijay');
+    self.lastName = ko.observable('Bhaskar');
+
+    self.fullName = ko.computed(function() {
+        return self.firstName() + " " + self.lastName();
+    }, this);
+
+    self.todos = ko.observableArray([{
+        item: "One"
+    }, {
+        item: "Two"
+    }]);
+}
+
+ko.applyBindings(new AppViewModel());
