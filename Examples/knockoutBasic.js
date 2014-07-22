d201c68c91a3233194c5a04dc783f8bf84594d5a
Vijay Bhaskar <vijaymca@sify.com>
2014-07-22 20:02:27 +0530

    Knockout
    Array

--- Examples/knockoutBasic.js
+++ Examples/knockoutBasic.js
@@ -1,28 +1,37 @@
 var myViewModel = {
-    personName: ko.observable('Vijay Bhaskar'),
-    personAge: ko.observable(23)
+	personName : ko.observable('Vijay Bhaskar'),
+	personAge : ko.observable(23)
 };
 
 //ko.applyBindings(myViewModel);
 
 
-
 function AppViewModel() {
-    var self = this;
-    self.firstName = ko.observable('Vijay');
-    self.lastName = ko.observable('Bhaskar');
 
-    self.fullName = ko.computed(function() {
-        return self.firstName() + " " + self.lastName();
-    }, this);
+	var self = this;
+	self.firstName = ko.observable('Vijay');
+	self.lastName = ko.observable('Bhaskar');
 
-    self.todos = ko.observableArray([{
-        item: "One"
-    }, {
-        item: "Two"
-    }]);
+	self.fullName = ko.computed(function () {
+			return self.firstName() + " " + self.lastName();
+		}, this);
+
+	self.todos = ko.observableArray([{
+					item : "One"
+				}, {
+					item : "Two"
+				}
+			]);
+			
+	self.todoLen = self.todos().length;
 	
-	self.newTodo=ko.observable("");
+	self.newTodo = ko.observable();
+
+	self.addNewTodo = function () {
+		self.todos.push({item : self.newTodo()});
+		$('#lblCount').html(self.todos().length);
+		self.newTodo("");
+	}
 }
 
 ko.applyBindings(new AppViewModel());
